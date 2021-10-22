7cbba34512e86b3608f967df1007402b70557a73
Raghunandan Yadav <90599332+Raghu-N-Yadav@users.noreply.github.com>
2021-10-22 15:32:17 +0530

    example

--- nestedList.py
+++ nestedList.py
@@ -1,19 +1,18 @@
-'''Given the names and grades for each student in a class of  students,
- store them in a nested list and print the name(s) of any student(s) having the second lowest grade.'''
-
 if __name__ == '__main__':
     arr = []
     arr2  = []
     arr3 = []
+    #newL = []
 
-    for _ in range(int(input('Number of students ?? -'))):
-        name = input('Enter name ')
-        score = float(input('enter score '))
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
         arr = [name,score]
         arr2.append(arr)
         arr3.append(score)
-    arr3.sort()
-    x  = arr3[1]
+    newL = list(set(arr3)) #removing duplicates
+    newL.sort()
+    x  = newL[1]
     y  = []
     for val in arr2:
         if val[1] == x:
